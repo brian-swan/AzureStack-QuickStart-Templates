70e8afe1861f23c6d5ba3adac9a7f226388a6896
Brian Swan <bswan@microsoft.com>
2018-09-07 16:49:11 -0700

    sql auth in template

--- sql-2016-ha/UpdateSqlVms.ps1
+++ sql-2016-ha/UpdateSqlVms.ps1
@@ -36,12 +36,13 @@ else
 Get-Service SQLSERVERAGENT | Start-Service
 Invoke-Sqlcmd -InputFile "MaintenanceSolution.sql"
 
+# TODO: Remove this is the template works for creating the right user.
 # Create azbetsqluser login
-$createLogin = "CREATE Login $UserName WITH Password = '$Password', SID = 0x593F982D94683D4ABA61D4390CF67FC9, CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF"
+<#$createLogin = "CREATE Login $UserName WITH Password = '$Password', SID = 0x593F982D94683D4ABA61D4390CF67FC9, CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF"
 $addLoginToSysAdmin = "ALTER SERVER ROLE sysadmin ADD MEMBER $UserName"
 Invoke-Sqlcmd -Query $createLogin 
 Invoke-Sqlcmd -Query $addLoginToSysAdmin
-Restart-Service -Force MSSQLSERVER
+Restart-Service -Force MSSQLSERVER#>
 
 # Install AzBet Monitor
 if( $null -eq (Get-Module -ListAvailable -Name AzureRM.Storage))
