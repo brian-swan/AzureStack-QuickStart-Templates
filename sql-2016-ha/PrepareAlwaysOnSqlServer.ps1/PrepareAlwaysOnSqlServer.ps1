7e018aefdab090d74e5105fb6fb5a44a0a18c505
Brian Swan <bswan@microsoft.com>
2018-09-07 08:56:11 -0700

    in progress

---
+++ sql-2016-ha/PrepareAlwaysOnSqlServer.ps1/PrepareAlwaysOnSqlServer.ps1
@@ -0,0 +1,296 @@
+﻿#
+# Copyright="© Microsoft Corporation. All rights reserved."
+#
+
+configuration PrepareAlwaysOnSqlServer
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$SQLServicecreds,
+
+        [System.Management.Automation.PSCredential]$SQLAuthCreds,
+
+        [Parameter(Mandatory)]
+        [String]$SqlAlwaysOnEndpointName,
+
+        [UInt32]$DatabaseEnginePort = 1433,
+
+        [Parameter(Mandatory)]
+        [UInt32]$NumberOfDisks,
+
+        [Parameter(Mandatory)]
+        [String]$WorkloadType,
+
+        [String]$DomainNetbiosName=(Get-NetBIOSName -DomainName $DomainName),
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    Import-DscResource -ModuleName xComputerManagement,CDisk,xActiveDirectory,XDisk,xSql, xSQLServer, xSQLps,xNetworking, PSDesiredStateConfiguration
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Admincreds.UserName)", $Admincreds.Password)
+    [System.Management.Automation.PSCredential]$DomainFQDNCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    [System.Management.Automation.PSCredential]$SQLCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($SQLServicecreds.UserName)", $SQLServicecreds.Password)
+
+    $RebootVirtualMachine = $false
+
+    if ($DomainName)
+    {
+        $RebootVirtualMachine = $true
+    }
+
+    $NumberOfDisks = 2
+
+    #Finding the next avaiable disk letter for Add disk
+    $NewDiskLetters = ls function:[f-z]: -n | ?{ !(test-path $_) } | select -First $NumberOfDisks 
+
+    WaitForSqlSetup
+
+    Node localhost
+    {
+        xSqlCreateVirtualDataDisk NewVirtualDisk
+        {
+            NumberOfDisks = $NumberOfDisks
+            NumberOfColumns = $NumberOfDisks
+            DiskLetters = $NewDiskLetters
+            OptimizationType = $WorkloadType
+            StartingDeviceID = 2
+            RebootVirtualMachine = $RebootVirtualMachine
+        }
+
+        WindowsFeature FC
+        {
+            Name = "Failover-Clustering"
+            Ensure = "Present"
+            DependsOn = "[xSqlCreateVirtualDataDisk]NewVirtualDisk"
+        }
+
+        WindowsFeature FailoverClusterTools 
+        { 
+            Ensure = "Present" 
+            Name = "RSAT-Clustering-Mgmt"
+            DependsOn = "[WindowsFeature]FC"
+        } 
+
+        WindowsFeature FCPS
+        {
+            Name = "RSAT-Clustering-PowerShell"
+            Ensure = "Present"
+            DependsOn = "[WindowsFeature]FailoverClusterTools"
+        }
+
+        WindowsFeature ADPS
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+            DependsOn = "[WindowsFeature]FCPS"
+        }
+
+        xWaitForADDomain DscForestWait 
+        { 
+            DomainName = $DomainName 
+            DomainUserCredential= $DomainCreds
+            RetryCount = $RetryCount 
+            RetryIntervalSec = $RetryIntervalSec 
+	        DependsOn = "[WindowsFeature]ADPS"
+        }
+        
+        xComputer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $DomainName
+            Credential = $DomainCreds
+	        DependsOn = "[xWaitForADDomain]DscForestWait"
+        }
+
+        xFirewall DatabaseEngineFirewallRule
+        {
+            Direction = "Inbound"
+            Name = "SQL-Server-Database-Engine-TCP-In"
+            DisplayName = "SQL Server Database Engine (TCP-In)"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Engine."
+            DisplayGroup = "SQL Server"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = $DatabaseEnginePort -as [String]
+            Ensure = "Present"
+            DependsOn = "[xComputer]DomainJoin"
+        }
+
+        xFirewall DatabaseMirroringFirewallRule
+        {
+            Direction = "Inbound"
+            Name = "SQL-Server-Database-Mirroring-TCP-In"
+            DisplayName = "SQL Server Database Mirroring (TCP-In)"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Mirroring."
+            DisplayGroup = "SQL Server"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = "5022"
+            Ensure = "Present"
+            DependsOn = "[xComputer]DomainJoin"
+        }
+
+        xFirewall ListenerFirewallRule
+        {
+            Direction = "Inbound"
+            Name = "SQL-Server-Availability-Group-Listener-TCP-In"
+            DisplayName = "SQL Server Availability Group Listener (TCP-In)"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Availability Group listener."
+            DisplayGroup = "SQL Server"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = "59999"
+            Ensure = "Present"
+            DependsOn = "[xComputer]DomainJoin"
+        }
+
+        xSqlLogin AddDomainAdminAccountToSysadminServerRole
+        {
+            Name = $DomainCreds.UserName
+            LoginType = "WindowsUser"
+            ServerRoles = "sysadmin"
+            Enabled = $true
+            Credential = $Admincreds
+            DependsOn = "[xComputer]DomainJoin"
+        }
+
+        xADUser CreateSqlServerServiceAccount
+        {
+            DomainAdministratorCredential = $DomainCreds
+            DomainName = $DomainName
+            UserName = $SQLServicecreds.UserName
+            Password = $SQLServicecreds
+            Ensure = "Present"
+            DependsOn = "[xSqlLogin]AddDomainAdminAccountToSysadminServerRole"
+        }
+
+        xSqlLogin AddSqlServerServiceAccountToSysadminServerRole
+        {
+            Name = $SQLCreds.UserName
+            LoginType = "WindowsUser"
+            ServerRoles = "sysadmin"
+            Enabled = $true
+            Credential = $Admincreds
+            DependsOn = "[xADUser]CreateSqlServerServiceAccount"
+        }
+        
+        xSqlTsqlEndpoint AddSqlServerEndpoint
+        {
+            InstanceName = "MSSQLSERVER"
+            PortNumber = $DatabaseEnginePort
+            SqlAdministratorCredential = $Admincreds
+            DependsOn = "[xSqlLogin]AddSqlServerServiceAccountToSysadminServerRole"
+        }
+
+        xSQLServerStorageSettings AddSQLServerStorageSettings
+        {
+            InstanceName = "MSSQLSERVER"
+            OptimizationType = $WorkloadType
+            DependsOn = "[xSqlTsqlEndpoint]AddSqlServerEndpoint"
+        }
+
+        xSqlServer ConfigureSqlServerWithAlwaysOn
+        {
+            InstanceName = $env:COMPUTERNAME
+            SqlAdministratorCredential = $Admincreds
+            ServiceCredential = $SQLCreds
+            MaxDegreeOfParallelism = 1
+            FilePath = $NewDiskLetter[0][0] + ":\DATA"
+            LogPath = $NewDiskLetter[1][0] + ":\LOG"
+            DomainAdministratorCredential = $DomainFQDNCreds
+            EnableTcpIp = $true
+            DependsOn = "[xSQLServerStorageSettings]AddSQLServerStorageSettings"
+        }
+
+        Script EnableContainedDB
+        {
+            SetScript = 
+            { 
+                $tempPath = "$env:TEMP\sqlcmd.sql"
+
+                Set-Content -Path $tempPath -Value "EXEC sp_configure 'contained database authentication', 1 `r`n GO `r`n RECONFIGURE `r`n GO"
+
+                Import-Module sqlps -Force
+
+                Invoke-Sqlcmd -InputFile $tempPath
+
+            }
+
+            TestScript = 
+            {
+                Import-Module sqlps -Force
+
+                $getCommand = "SELECT * FROM sys.configurations"
+
+                $containedDBValue = (Invoke-Sqlcmd -Query $getCommand | ? {$_.name -eq "contained database authentication"}).value
+
+                return $containedDBValue -eq 1
+            }
+
+            GetScript = 
+            {
+                Import-Module sqlps -Force
+
+                $getCommand = "SELECT * FROM sys.configurations"
+
+                return (Invoke-Sqlcmd -Query $getCommand | ? {$_.name -eq "contained database authentication"}).value
+            }          
+        }
+
+        LocalConfigurationManager 
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+    }
+}
+function Get-NetBIOSName
+{ 
+    [OutputType([string])]
+    param(
+        [string]$DomainName
+    )
+
+    if ($DomainName.Contains('.')) {
+        $length=$DomainName.IndexOf('.')
+        if ( $length -ge 16) {
+            $length=15
+        }
+        return $DomainName.Substring(0,$length)
+    }
+    else {
+        if ($DomainName.Length -gt 15) {
+            return $DomainName.Substring(0,15)
+        }
+        else {
+            return $DomainName
+        }
+    }
+}
+function WaitForSqlSetup
+{
+    # Wait for SQL Server Setup to finish before proceeding.
+    while ($true)
+    {
+        try
+        {
+            Get-ScheduledTaskInfo "\ConfigureSqlImageTasks\RunConfigureImage" -ErrorAction Stop
+            Start-Sleep -Seconds 5
+        }
+        catch
+        {
+            break
+        }
+    }
+}
